1a44fc3f44d83b206fefa6b64e7563646103633e
wxw-matt <matt.wxw.adelaide@gmail.com>
2022-02-11 19:53:37 +1030

    add helper class for testing

---
+++ tests/helper.py
@@ -0,0 +1,60 @@
+import os, unittest
+from os import path
+from pathlib import Path
+import yaml
+
+def _current_dir():
+    return path.dirname(path.abspath(__file__))
+
+class Helper:
+    def __init__(self, basename, current_dir=_current_dir()):
+        self._current_dir = current_dir
+        self._project_name = '%s_%s' % (basename, os.getpid())
+        self._project_path = Path(self._current_dir, self._project_name)
+        self._config_path = Path(self._project_path, 'config')
+        self._project_path.mkdir(parents=True, exist_ok=True)
+        self._config_path.mkdir(parents=True, exist_ok=True)
+
+    @property
+    def project_name(self):
+        return self._project_name
+
+    @property
+    def project_path(self):
+        return self._project_path
+
+    @property
+    def config_path(self):
+        return self._config_path
+
+    def make_project_dirs(self):
+        self._project_path.mkdir(parents=True, exist_ok=True)
+        self._config_path.mkdir(parents=True, exist_ok=True)
+
+    def remove_dirs(self, path=None):
+        path = path or self.project_path
+        for child in path.iterdir():
+            if child.is_dir():
+                for gc in child.iterdir():
+                    gc.unlink()
+                child.rmdir()
+            else:
+                child.unlink()
+        path.rmdir()
+
+class TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._helper = Helper('myapp')
+
+    def setUp(self):
+        self._helper.make_project_dirs()
+
+    def tearDown(self):
+        self._helper.remove_dirs()
+
+
+if __name__ == '__main__':
+    helper = Helper('app')
+    helper.make_project_dirs()
+    helper.remove_dirs()
--- tests/test_template.py
+++ tests/test_template.py
@@ -2,41 +2,37 @@ import os, unittest
 from os import path
 from pathlib import Path
 from lib import rails_cmds
+from tests import helper
 import yaml
 
 
-def remove_dirs(path):
-    for child in path.iterdir():
-        if child.is_dir():
-            for gc in child.iterdir():
-                gc.unlink()
-            child.rmdir()
-        else:
-            child.unlink()
-    path.rmdir()
 
-class TestDataConfigTemplate(unittest.TestCase):
-    def setUp(self):
-        self.current_dir = path.dirname(path.abspath(__file__))
-        self.project_name = 'myapp_%s' % os.getpid()
-        self.project_path = Path(self.current_dir, self.project_name)
-        self.config_path = Path(self.project_path, 'config')
-        self.project_path.mkdir(parents=True, exist_ok=True)
-        self.config_path.mkdir(parents=True, exist_ok=True)
-
-    def tearDown(self):
-        remove_dirs(self.project_path)
-
-    def test_customer_count(self):
-        rails_cmds.create_files_for_the_project('rails-tag:1.0','mysql', str(self.project_path))
-        f = open(self.config_path.joinpath('database.yml'))
+class TestGeneratedFiles(helper.TestCase):
+    def test_files(self):
+        rails_cmds.create_files_for_the_project('rails-tag:1.0','mysql', str(self._helper.project_path))
+        f = open(self._helper.config_path.joinpath('database.yml'))
         config = yaml.safe_load(f)
         f.close()
         password = 'example'
         for env in ['development', 'production']:
-            self.assertEqual(config[env]['database'], f'{self.project_name}_{env}')
+            self.assertEqual(config[env]['database'], f'{self._helper.project_name}_{env}')
             self.assertEqual(config[env]['host'], f'db')
             self.assertTrue(password in config[env]['password'])
+
+        f = open(self._helper.project_path.joinpath('docker-compose.yml'))
+        docker_compose = yaml.safe_load(f)
+        f.close()
+
+        web = docker_compose['services']['web']
+        db = docker_compose['services']['db']
+        self.assertTrue("bundle exec rails s -p 3000 -b '0.0.0.0'" in web['command'])
+        self.assertTrue("mariadb" in db['image'])
+
+        self.assertTrue(Path(self._helper.project_path.joinpath('rod')).is_symlink())
+
+        with open(self._helper.project_path.joinpath('Dockerfile')) as f:
+            dockerfile = f.read()
+            self.assertTrue('RUN bundle install' in dockerfile)
 
 if __name__ == '__main__':
     unittest.main()
